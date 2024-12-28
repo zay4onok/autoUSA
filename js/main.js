fb3b21ceaa095bc7f80565c805644060c6eb690a
Dmitry <dmitronus2015@gmail.com>
2024-12-28 09:49:46 +0200

    add cars js breakpoint

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ $(function () {
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 675,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 475,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
